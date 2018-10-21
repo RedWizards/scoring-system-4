80244ae0fdc710b247659d163904cf133446c857
Red Periabras <redperiabras@gmail.com>
2018-10-21 10:31:50 +0800

    Add missing connection file

---
+++ database/connection.php
@@ -0,0 +1,12 @@
+
+<?php
+	$host = "127.0.0.1";
+	$user = "root";
+	$password = "";
+	$dbName = "scoring";
+	$port = 3306;
+	$conn = new mysqli($host, $user, $password, $dbName, $port);
+	if($conn->connect_errno){
+		echo "Failed to connect to MySQL: ( ".$conn->connect_errno ." ) ".$conn->connect_error;
+	}
+?>
